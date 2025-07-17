44fdac0375bbda6e66f6ef3c06a55d43db1954cb
johnycodestone <mwaqarsaleem1@gmail.com>
2025-07-17 19:59:20 +0500

    Resolved some bugs in footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,22 +2,27 @@ import React from "react";
 import Button from "./Button";
 
 const Footer = () => {
-    return (
-        <div id="footer">
-            <div className="footer-text">
-                <h2 className="footer-h">Let`s Connect</h2>
-                <p className="footer-p">Ready to collaborate or just want to get in touch, I`d love to hear from you!</p>
-            </div>
-
-            <ul id="social-icons">
-                <li>LinkedIn</li>
-                <li>Email</li>
-                <li>GitHub</li>
-            </ul>
-
-            <Button text="Download Resume"/>
+  return (
+    <section id="footer-box">
+      <div id="footer">
+        <div className="footer-text">
+          <h2 className="footer-h">Let`s Connect</h2>
+          <p className="footer-p">
+            Ready to collaborate or just want to get in touch, I`d love to hear
+            from you!
+          </p>
         </div>
-    )
-}
+
+        <ul id="social-icons">
+          <li>LinkedIn</li>
+          <li>Email</li>
+          <li>GitHub</li>
+        </ul>
+
+        <Button text="Download Resume" />
+      </div>
+    </section>
+  );
+};
 
 export default Footer;
